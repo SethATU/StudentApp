7d59de403f942a1d473ffe66a34c0fa7f648893c
SethButler-STUDENT <g00467621@atu.ie>
2025-10-06 10:22:36 +0100

    constructors and aarray list

--- StudentApp/src/main/java/ie/atu/Student.java
+++ StudentApp/src/main/java/ie/atu/Student.java
@@ -1,4 +1,35 @@
 package ie.atu;
 
 public class Student {
+    String name, email, course;
+
+    public Student(String name, String email, String course) {
+        name = " ";
+        email = " ";
+        course = " ";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
